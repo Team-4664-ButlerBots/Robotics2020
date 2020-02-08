8e8bcce1e07b224b3790f1c9ea6bdb81ddd56bd9
nathansand <natd6623@gmail.com>
2020-02-08 10:07:02 -0600

    Simplified and added notes

--- src/main/java/frc/robot/ControllerManager.java
+++ src/main/java/frc/robot/ControllerManager.java
@@ -16,30 +16,23 @@ public class ControllerManager {
     
     private Joystick gamepad = new Joystick(0);
     private Joystick joystick = new Joystick(1);
-    int speeddiv = 1;
+    int speeddiv = 1; //Speed to divide both sides by
 
     public double[] driveControls(){
         double[] input = new double[2];
 
-
-        if (gamepad.getRawButtonPressed(5)) {
+        if (gamepad.getRawButtonPressed(5)) { //Toggle speed between fast and slow if left bumper (5) is pressed
             if (speeddiv == 1) {
-                speeddiv = 2;
+                speeddiv = 2; //Set slow
             } else {
-                speeddiv = 1;
+                speeddiv = 1; //Set fast
             }
         }
         
-        input[1] = gamepad.getRawAxis(1)/speeddiv;
-        input[0] = gamepad.getRawAxis(3)/speeddiv;
+        //Set inputs
+        input[1] = gamepad.getRawAxis(1) / speeddiv;
+        input[0] = gamepad.getRawAxis(3) / speeddiv;
 
         return input;
-    }
-
-    public boolean getBruh(){ //Change speed if left bumper (button 5) pressed
-        if (gamepad.getRawButtonPressed(5)) {
-            return true;
-        }
-        return false;
     }
 }
